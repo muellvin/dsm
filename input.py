ae40ee94fd45be9d22da1fb1348a084d19e155c4
ubuntu15 <login@peterkuhn.ch>
2023-02-06 18:36:39 +0100

    input works

--- input.py
+++ input.py
@@ -1,6 +1,13 @@
 import numpy as np
 
+#run this script in the begining of the programm
+#afterwards the arrays nodes, node_supp, node_disp, f_ext, beam_nodes, beam_supp, beam_prop
+#will have been intiialisend and filled with values
+#eighter by and hard coded example or by a user input
+
 example_choice = int(input("Woud you like to use a standart example?: 0-2 are basic, 3-4 are usefull, 5 is free user input"))
+
+#the first 4 are hard coded examples
 if(example_choice==0):
     print("This is the basic example, one beam fixed on both ends")
     nodes = np.array([[0,0], [1,0]])
@@ -65,12 +72,12 @@ elif(example_choice==4):
     beam_supp = np.array([[1, 1, 0, 1, 1, 1],[1, 1, 0, 1, 1, 1],[1, 1, 0, 1, 1, 1],[1, 1, 0, 1, 1, 1]])
     beam_prop = np.array([[1, 1, 1, 1,0],[1, 1, 1, 1,0],[1, 1, 1, 1, np.pi()*-0.5],[1, 1, 1, 1, np.pi()*-0.5]])
 
+#this is the user interface
 elif(example_choice==5):
-    number_of_nodes = int(input("how many nodes do you need?"))
 
-    #make a declaration of the later used things, it should all be overwritten
-    nodes = np.array([[0,0], [0,0]])
-    node_supp = np.array([[0,0,0],[0,0,0]])
+    #make a declaration of the later used things, it should all be overwritten and appended to
+    nodes = np.array([[0,0]])
+    node_supp = np.array([[0,0,0]])
 
     node_disp = np.array([0,0,0, 0,0,0])
     f_ext = np.array([0,0,0, 0,0,0])
@@ -79,32 +86,35 @@ elif(example_choice==5):
     beam_nodes = np.array([[0,0]])
     beam_supp = np.array([[0, 0, 0, 0, 0, 0]])
     beam_prop = np.array([[1, 1, 1, 1,0]])
+
     
+    number_of_nodes = int(input("how many nodes do you need?"))
     for i in range(number_of_nodes):
         print("node number: ", i)
-        np.append(nodes, [0,0])
+        nodes = np.append(nodes, [[0,0]], axis=0)#this should be one too many
+        print(nodes)
         nodes[i][0] = int(input("X Position"))
         nodes[i][1] = int(input("Y Position"))
 
-        np.append(node_supp,[1,1,1])
+        node_supp = np.append(node_supp,[[1,1,1]], axis=0)
         node_supp[i][0] = bool(input("X direction fixed? 0 if not"))
         node_supp[i][1] = bool(input("Y direction fixed? 0 if not"))
         node_supp[i][2] = bool(input("torque fixed? 0 if not"))
 
-    node_disp = np.zeros(number_of_nodes)
+    node_disp = np.zeros(number_of_nodes)#this will be later used, and filled by the computer
 
-    number_of_forces = int(input("how many forces do you realy need?"))
+    number_of_forces = int(input("how many forces do you really need?"))
     for i in range(number_of_forces):
         print("force numer :", i)
         force_node_number = int(input("on wich node does the force act?"))
         force_x_direction = float(input("X direction, put 0 if not"))
         force_y_direction = float(input("Y direction"))
 
-        np.append(f_ext, [0,0])
-        f_ext[force_node_number*3 -1]= force_x_direction
-        f_ext[force_node_number*3 ]= force_y_direction
+        f_ext =np.append(f_ext, [0,0,0], axis=0)#the unused third 0 is the moment
+        f_ext[force_node_number*3 ]= force_x_direction
+        f_ext[force_node_number*3 +1]= force_y_direction#this should be the correct position in the long array
 
-    number_of_beams = int(input("how many beams are there?"))
+    number_of_beams = int(input("how many beams are there? Count twise, programme once!"))
     for i in range(number_of_beams):
         print("beam number: ", i)
         begin_of_beam = int(input("number of begining node"))
@@ -117,9 +127,12 @@ elif(example_choice==5):
         beam_supp_2_Y = bool(input("support of end node in Y direction"))
         beam_supp_2_T = bool(input("support of end node in Torsion direction"))
 
-        np.append(beam_nodes, [0,0,0,0,0,0])
+        
+        beam_nodes = np.append(beam_nodes, [[0,0]], axis=0)
         beam_nodes[i][0] = begin_of_beam
         beam_nodes[i][1] = end_of_beam
+        
+        beam_supp = np.append(beam_supp, [[0,0,0,0,0,0]], axis=0)
         beam_supp[i][0]= beam_supp_1_X
         beam_supp[i][1]= beam_supp_1_Y
         beam_supp[i][2]= beam_supp_1_T
@@ -127,11 +140,12 @@ elif(example_choice==5):
         beam_supp[i][4]= beam_supp_2_Y
         beam_supp[i][5]= beam_supp_2_T
 
+        #let's give this monster a try
         beam_angle_Diff_X = nodes[beam_nodes[i][1]][0]-nodes[beam_nodes[i][0]][0]
         beam_angle_Diff_Y = nodes[beam_nodes[i][1]][1]-nodes[beam_nodes[i][0]][1]
         beam_angle = np.arctan(beam_angle_Diff_Y/ beam_angle_Diff_X)
 
-        np.appned(beam_prop, [1,1,1,1,0])
+        np.append(beam_prop, [[1,1,1,1,0]])
         beam_prop[i]=[1,1,1,1, beam_angle]
 else:
     print("Wronge choice, go home")
