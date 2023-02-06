8b9005e168a18ad9d404db3d9cc848acf9cdc510
Vinzenz Mueller <vam8238@gmail.com>
2023-02-06 14:27:00 +0100

    test2

--- test.py
+++ test.py
@@ -2,23 +2,28 @@ import numpy as np
 
 nodes = np.array([[0,0], [1,0], [2,0]])
 
-node_dis = np.array([[0,0,0],[0,0,0],[0,0,0]])
-f_ext = np.array([[0,0,0],[0,-1,0],[0,0,0]])
-p_int = np.array([[0,0,0],[0,0,0],[0,0,0]])
+node_disp = np.array([0,0,0, 0,0,0, 0,0,0])
+f_ext = np.array([0,0,0, 0,-1,0, 0,0,0])
+#p_int = np.array([[0,0,0],[0,0,0],[0,0,0]])
 
 beam_nodes = np.array([[0,1],[1,2]])
-beam_supp = np.array([[1, 1, 1, 1, 1, 0],[1, 1, 0, 1, 1, 0]])
+beam_supp = np.array([[1, 1, 1, 1, 1, 0],[1, 1, 0, 1, 1, 1]])
+beam_prop = np.array([[A, E , I, L],[]])
 
 #building the stiffness matrixes of each beam
 beam_k_loc = np.array((len(beam_nodes), 6, 6))
+beam_k_glob = beam_k_loc
 
 for i in range(len(beam_nodes)):
-    k_loc = np.array([[1,0,0,-1,0,0],[0,1,1,0,-1,1],[0,1,1,0,-1,1],[-1,0,0,1,0,0],[0,-1,-1,0,1,-1],[0,1,1,0,-1,1]])
-    for j in range(0,3):
-        k_loc = k_loc[j]*beam_supp
-    #then also rotating
-    #then adding
-    beam_k_loc[i] = k_loc
+    if (beam_supp[i] == [1, 1, 1, 1, 1, 0]):
+        beam_k_loc[i] = a
+    else if (beam_supp[i] == [1, 1, 0, 1, 1, 1]):
+        abs
+    else if (beam_supp[i] == [1, 1, 1, 1, 1, 1]):
+        abs
+    else
+        falsefalsefalse
+
 
 
 k_sys = np.zeros((3*len(nodes),3*len(nodes)))
