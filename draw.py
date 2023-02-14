c45beb6a90b839b55b48675cc86c6d30576e6837
ubuntu15 <login@peterkuhn.ch>
2023-02-14 12:58:41 +0100

    print_support_beams, it looks ugly but well

--- draw.py
+++ draw.py
@@ -52,10 +52,29 @@ def print_support_nodes(node_supp, nodes):
 def print_support_beams(beam_supp, beam_nodes, number_of_beams, nodes):
     #cycle throuth all beams
     for i in range(number_of_beams):
+#begining node
+        #completly fixed
         if beam_supp[i][0]==1 and node_supp[i][1]==1 and node_supp[i][2]==1:
             #print(nodes[i][0])#this looks like a debug thing
-            plt.scatter(nodes[beam_nodes[i][0]][0], nodes[beam_nodes[i][1]][1],s=None, c='blue', marker='s')
-
-        if node_supp[i][0]==1 and node_supp[i][1]==1 and node_supp[i][2]==0:
+            plt.scattern(nodes[beam_nodes[i][0]][0], nodes[beam_nodes[i][1]][1],s=None, c='blue', marker='s')
+        #torsion allowed
+        if beam_supp[i][0]==1 and beam_supp[i][1]==1 and beam_supp[i][2]==0:
             print(nodes[i][0])
-            plt.scatter(nodes[i][0], nodes[i][1],s=None, c='blue', marker='o')
+            plt.scatter(nodes[beam_nodes[i][0]][0], nodes[beam_nodes[i][1]][1],s=None, c='blue', marker='o')
+        #free
+        if beam_supp[i][0]==0 and beam_supp[i][1]==0 and beam_supp[i][2]==0:
+            print(nodes[i][0])
+            plt.scatter(nodes[beam_nodes[i][0]][0], nodes[beam_nodes[i][1]][1],s=None, c='blue', marker='o')#use a different marker for free node
+#endinging node
+        #completly fixed
+        if beam_supp[i][3]==1 and node_supp[i][4]==1 and node_supp[i][5]==1:
+            #print(nodes[i][0])#this looks like a debug thing
+            plt.scattern(nodes[beam_nodes[i][0]][0], nodes[beam_nodes[i][1]][1],s=None, c='blue', marker='s')
+        #torsion allowed
+        if beam_supp[i][3]==1 and beam_supp[i][4]==1 and beam_supp[i][5]==0:
+            print(nodes[i][0])
+            plt.scatter(nodes[beam_nodes[i][0]][0], nodes[beam_nodes[i][1]][1],s=None, c='blue', marker='o')
+        #free
+        if beam_supp[i][3]==0 and beam_supp[i][4]==0 and beam_supp[i][5]==0:
+            print(nodes[i][0])
+            plt.scatter(nodes[beam_nodes[i][0]][0], nodes[beam_nodes[i][1]][1],s=None, c='blue', marker='o')#use a different marker for free node
